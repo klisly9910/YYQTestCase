d6c4d03fbcf7a5be8780137939e3682e20489e83
klisly9910 <648820965@qq.com>
2016-05-25 10:17:56 +0800

    我的页面

---
+++ src/com/txh/yyq/my/MyPage.java
@@ -0,0 +1,34 @@
+package com.txh.yyq.my;
+
+import com.android.uiautomator.core.UiObject;
+import com.android.uiautomator.core.UiSelector;
+import com.android.uiautomator.testrunner.UiAutomatorTestCase;
+
+public class MyPage extends UiAutomatorTestCase {
+	public static UiObject loggedIcon = new UiObject(
+			new UiSelector().resourceId("com.mappn.gfan:id/logged_iv_icon"));
+	public static UiObject loggedName = new UiObject(
+			new UiSelector().resourceId("com.mappn.gfan:id/logged_tv_name"));
+	public static UiObject luckyTicket = new UiObject(
+			new UiSelector()
+					.resourceId("com.mappn.gfan:id/logged_tv_luck_coin_num"));
+	public static UiObject chargeBtn = new UiObject(
+			new UiSelector().resourceId("com.mappn.gfan:id/logged_tv_charge"));
+	public static UiObject runningBtn = new UiObject(
+			new UiSelector().resourceId("com.mappn.gfan:id/logged_tv_running"));
+	public static UiObject resultBtn = new UiObject(
+			new UiSelector().resourceId("com.mappn.gfan:id/logged_tv_result"));
+	public static UiObject allrecords = new UiObject(
+			new UiSelector()
+					.resourceId("com.mappn.gfan:id/logged_tv_all_records"));
+	public static UiObject awardrecords = new UiObject(
+			new UiSelector()
+					.resourceId("com.mappn.gfan:id/logged_tv_award_records"));
+	public static UiObject chargerecords = new UiObject(
+			new UiSelector()
+					.resourceId("com.mappn.gfan:id/logged_tv_charge_records"));
+	public static UiObject aboutyyq = new UiObject(
+			new UiSelector()
+					.resourceId("com.mappn.gfan:id/logged_tv_about_yyq"));
+
+}
