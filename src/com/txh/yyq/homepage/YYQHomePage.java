3f482d5bbd1ac906c052b7c5143abde4f41b38a2
klisly9910 <648820965@qq.com>
2016-05-25 10:16:34 +0800

    一元抢首页

---
+++ src/com/txh/yyq/homepage/YYQHomePage.java
@@ -0,0 +1,13 @@
+package com.txh.yyq.homepage;
+
+import com.android.uiautomator.testrunner.UiAutomatorTestCase;
+
+/**
+ * 一元抢首页（属性） 商品信息属性与首页中的商品信息属性一致，测试方法直接调用首页的商品信息属性即可
+ * 
+ * @param args
+ */
+public class YYQHomePage extends UiAutomatorTestCase {
+	
+
+}
