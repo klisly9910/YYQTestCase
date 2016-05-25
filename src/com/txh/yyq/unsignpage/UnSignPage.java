d00ac1a5460d8becf04637a983c2a2921cd83879
klisly9910 <648820965@qq.com>
2016-05-25 10:22:06 +0800

    未登录页面

---
+++ src/com/txh/yyq/unsignpage/UnSignPage.java
@@ -0,0 +1,34 @@
+package com.txh.yyq.unsignpage;
+
+import com.android.uiautomator.core.UiObject;
+import com.android.uiautomator.core.UiSelector;
+import com.android.uiautomator.testrunner.UiAutomatorTestCase;
+
+/**
+ * 未登录页面（属性）
+ * 
+ * @author xiaohua
+ * 
+ */
+public class UnSignPage extends UiAutomatorTestCase {
+	public static UiObject etusername = new UiObject(
+			new UiSelector().resourceId("com.mappn.gfan:id/et_user_name"));
+	public static UiObject etpsd = new UiObject(
+			new UiSelector().resourceId("com.mappn.gfan:id/et_user_psd"));
+	public static UiObject forgetpsd = new UiObject(
+			new UiSelector().resourceId("com.mappn.gfan:id/tv_forget_psd"));
+	public static UiObject signBtn = new UiObject(
+			new UiSelector().resourceId("com.mappn.gfan:id/tv_sign_in"));
+	public static UiObject weichat = new UiObject(
+			new UiSelector().resourceId("com.mappn.gfan:id/iv_login_weichat"));
+	public static UiObject loginQQ = new UiObject(
+			new UiSelector().resourceId("com.mappn.gfan:id/iv_login_QQ"));
+	public static UiObject register = new UiObject(
+			new UiSelector().resourceId("com.mappn.gfan:id/tv_register"));
+	
+	// 微信/QQ登录授权
+	public static UiObject btnOk = new UiObject(
+			new UiSelector().className("android.widget.Button"));
+	
+
+}
